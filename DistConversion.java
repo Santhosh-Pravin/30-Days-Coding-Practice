7604d61c2abe218b5b452202eabbf745930cc468
Santhosh Pravin <santypravin3@gmail.com>
2026-06-17 11:44:33 +0530

    Day 6

---
+++ DistConversion.java
@@ -0,0 +1,10 @@
+class DistConversion {
+    public static void main(String[] args) {
+        System.out.println("Kilometers to Miles");
+        
+        double kilometers = 10.8;
+        double miles = kilometers / 1.6; // Corrected conversion factor
+        
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+}
