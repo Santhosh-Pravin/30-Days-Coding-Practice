54b0d239117edd02de6f41066859975d69a0bae1
Santhosh Pravin <santypravin3@gmail.com>
2026-06-17 11:45:54 +0530

    Day 6

---
+++ Age.java
@@ -0,0 +1,10 @@
+class Age {
+    public static void main(String[] args) {
+        System.out.println("Age of Harry");
+        int birthyear_harry = 2000; // Birth year of Harry
+        int currentyear = 2024; // Current year
+        int age_harry = currentyear - birthyear_harry;
+
+        System.out.println("Harry's age in the year 2024 is: " + age_harry);
+    }
+}
