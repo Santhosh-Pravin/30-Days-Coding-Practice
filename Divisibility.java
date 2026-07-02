54441b0684bf27d9fe1ad7b92b3dc0c8f5904043
Santhosh Pravin <santypravin3@gmail.com>
2026-07-02 11:18:58 +0530

    Day 18

--- Divisibility.java
+++ Divisibility.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 
-public class Divisibility {
+class Divisibility {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter any integer: ");
+        
+        System.out.print("Enter a number: ");
         int num = input.nextInt();
-        if (num % 3 == 0) {
-            System.out.print("The number is divisible by 3");
+        
+        if (num % 5 == 0) { // Divisibility by 5
+            System.out.println("Is the number " + num + " divisible by 5? Yes");
         } else {
-            System.out.print("The number is not divisible by 3");
+            System.out.println("Is the number " + num + " divisible by 5? No");
         }
-
+        
+        input.close();
     }
 }
-
