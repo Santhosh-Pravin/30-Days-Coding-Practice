cf8d8327a74181b44abf79f7ecee4301b63a4f39
Santhosh Pravin <santypravin3@gmail.com>
2026-07-03 10:21:20 +0530

    Day 19

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -1,36 +1,16 @@
-class Multi5 extends Thread {
-	public void run() {
-		for(int i=0;i<10;i++){
-			int multiple = (i+1)*5;
-			System.out.println((i+1)+" x 5 = "+multiple);
-			try { 
-			    Thread.sleep(500); 
-			} 
-			catch (InterruptedException e) { 
-			    e.printStackTrace(); 
-			}
-		}
-	}
-}
-class Multi10 extends Thread {
-	public void run() {
-		for(int i=0;i<10;i++){
-			int multiple = (i+1)*10;
-			System.out.println((i+1)+" x 10 = "+multiple);
-			try { 
-			    Thread.sleep(500); 
-			} 
-			catch (InterruptedException e) { 
-			    e.printStackTrace(); 
-			}
-		}
-	}
-}
-public class MultiplicationTable {
-	public static void main(String[] args) {
-		Multi5 t1 = new Multi5();
-		Multi10 t2 = new Multi10();
-		t1.start();
-		t2.start();
-	}
+import java.util.Scanner;
+
+class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+
+        input.close();
+    }
 }
