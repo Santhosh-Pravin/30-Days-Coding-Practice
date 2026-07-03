d9e7294ddd7899bc8f2c96499aaefdd5aec6e2e8
Santhosh Pravin <santypravin3@gmail.com>
2026-07-03 10:21:01 +0530

    Day 19

---
+++ NumberCheck.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class NumberCheck {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+
+        // To check if the number is positive, negative, or zero
+        if (num > 0) {
+            System.out.println("The number is positive.");
+        } else if (num < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+
+        input.close();
+    }
+}
