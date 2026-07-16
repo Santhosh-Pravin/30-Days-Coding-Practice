5a5dde885eb66ba47efb2202941a48b5019e62c7
Santhosh Pravin <santypravin3@gmail.com>
2026-07-16 10:39:09 +0530

    Day 28

--- Calculator.java
+++ Calculator.java
@@ -1,41 +1,27 @@
-import java.util.Scanner;
-
-public class Calculator {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        
-        System.out.print("Enter number 1: ");
-        double first = input.nextDouble();
-        
-        System.out.print("Enter number 2: ");
-        double second = input.nextDouble();
-        
-        System.out.print("Enter operator (+, -, *, x, /): ");
-        String op = input.next();
-        
-        switch (op) {
-            case "+":
-                System.out.println("Addition: " + (first + second));
-                break;
-            case "-":
-                System.out.println("Subtraction: " + (first - second));
-                break;
-            case "*":
-            case "x":  // Handle both '*' and 'x'
-                System.out.println("Multiplication: " + (first * second));
-                break;
-            case "/":
-                if (second == 0) {
-                    System.out.println("Error: Division by zero is not allowed.");
-                } else {
-                    System.out.println("Division: " + (first / second));
-                }
-                break;
-            default:
-                System.out.println("Invalid Operator.");
-                break;
-        }
-        
-        input.close(); // Close scanner
+public class Calculator{
+    int a;
+    int b;
+    public int Add(int a,int b){
+        System.out.println("The sum of the given numbers is"+(a+b));
+        return a+b;
+    }
+    public int Substraction(int a,int b){
+        System.out.println("The difference of the given numbers is"+ (a-b));
+        return a-b;
+    }
+    public int Multiply(int a,int b){
+        System.out.println("The multiplication between given numbers is"+ (a*b));
+        return a*b;
+    }
+    public int Divide(int a,int b){
+        System.out.println("The quotient between given numbers is"+ (a/b));
+		return a/b;
+    }
+    public static void main(String[] args){
+        Calculator obj = new Calculator();
+        obj.Add(8,4);
+        obj.Substraction(8,4);
+        obj.Multiply(8,4);
+        obj.Divide(8,4);
     }
 }
